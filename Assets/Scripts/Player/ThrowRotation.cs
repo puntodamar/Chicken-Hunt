b55f41dd2f47ff4e3f4dad7870c5a5f44e0b0ea4
Unknown <punto.damar2@gmail.com>
2018-04-07 17:24:10 +0700

    kamera cctv
    - rotate
    - fov

---
+++ Assets/Scripts/Player/ThrowRotation.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+	public class ThrowRotation : MonoBehaviour
+	{
+		private bool _rotate;
+
+		void Update()
+		{
+			Vector3 vel = GetComponent<Rigidbody>().velocity;
+			if (_rotate)
+				transform.rotation = Quaternion.LookRotation(vel);
+		}
+
+		void OnCollisionEnter(Collision other)
+		{
+			_rotate = false;
+		}
+
+		void OnCollisionExit(Collision other)
+		{
+			_rotate = true;
+		}
+	}
+}
---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float RotateSpeed = 100f;
+    public bool PingPong = false;
+
+    [Range(0,360)]
+    public float PingPongAngle = 120f;
+
+    private float _currentAngle = 0;
+    
+    private void Update()
+    {
+        if(!PingPong)
+            transform.Rotate(Vector3.up, RotateSpeed * Time.deltaTime);
+        
+        else
+            //transform.Rotate(Vector3.up,Mathf.PingPong(Time.time * RotateSpeed,PingPongAngle));
+            transform.rotation = Quaternion.Euler(0f, Mathf.PingPong(Time.time * RotateSpeed, 90f), 0f);
+    }
+}
